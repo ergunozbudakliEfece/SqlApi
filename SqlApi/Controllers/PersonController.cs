af223c6b8f18256fb32acbd9483615c4f2a14e24
ergun <ergunozbudakli@gmail.com>
2022-03-24 01:01:26 +0300

    Proje dosyası ekle.

---
+++ SqlApi/Controllers/PersonController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+namespace SqlApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public PersonController(IConfiguration configuration) { 
+            _configuration = configuration; 
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"Select * From Persons";
+            DataTable table= new DataTable();
+            string sqldataSource = _configuration.GetConnectionString("con");
+            SqlDataReader sqlreader;
+            using (SqlConnection mycon = new SqlConnection(sqldataSource))
+            {
+                mycon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, mycon))
+                {
+                    sqlreader = myCommand.ExecuteReader();
+                    table.Load(sqlreader);
+                    sqlreader.Close();
+                    mycon.Close();
+                }
+            }
+            return new JsonResult(table); 
+        }
+    }
+}
---
+++ SqlApi/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace SqlApi.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+    }
+}
