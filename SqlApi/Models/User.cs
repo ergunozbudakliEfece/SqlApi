8a1b5bbc8597dc4928f2dfa50589335bfe699178
surec.gelistirme2 <efece641488>
2022-03-25 18:22:56 +0300

    stoklar eklendi

---
+++ SqlApi/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace SqlApi.Models
+{
+    public class User
+    {
+        public string UserID { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserRole { get; set; }
+        public bool Aktif { get; set; }
+        public string UserMail { get; set; }
+    }
+}
